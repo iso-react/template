7c25ea150b28de222c40115d715208bda574f310
Alex Henderson <alex@alexhenderson.me>
2019-05-11 14:41:26 -0700

    switch to using todo id to grab user to generate different results

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import User from '~/components/user';
 
-const Todo = ({id, userId, title, completed}) => {
+const Todo = ({id, title, completed}) => {
   return (
     <div>
       <div>{title}</div>
-      <User id={userId}>
+      {/* The JSON placeholder orders them by user.        */}
+      {/* Grabbing 10 will result in all the same creator. */}
+      {/* We are using the todo id to avoid this.          */}
+      <User id={id}>
         {(user, {loading, error}) => {
           if (loading) return 'loading';
           if (error) return error;

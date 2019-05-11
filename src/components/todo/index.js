cc9a3fa5f26a40d6453feac4fba2586050b657f7
Alex Henderson <alex@alexhenderson.me>
2019-05-11 13:58:30 -0700

    add basic emotion styles

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  width: '80%',
+  justifyContent: 'space-between',
+  margin: '0 auto 0',
+  '&:not(:nth-of-type(1))': {
+    borderTop: '1px solid black',
+  },
+};
+
+const nameStyle = {
+  display: 'block',
+};
+
+const statusStyle = {
+  display: 'block',
+  marginLeft: '1rem',
+};
 
 const Todo = ({id, userId, title, completed}) => {
-  return <div>{title}: {completed ? '✅' : '❌'}</div>
-}
+  return (
+    <div css={containerStyle}>
+      <div css={nameStyle}>{title}</div>
+      <div css={statusStyle}>{completed ? '✅' : '❌'}</div>
+    </div>
+  );
+};
 
 export default Todo;
